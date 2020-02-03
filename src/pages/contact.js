8ef83110e7458d911b39c1bd050ab56d5b0321cf
John Rickard <hello@johnrickard.me>
2020-02-03 23:52:59 +0100

    Add react-bootstrap
    * Added boostrap forms under /contact
    * Hooked forms up to Netlify Forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Layout from '../components/layout'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Contact = ({ data: { contact } }) => (
   <Layout>
@@ -15,6 +17,43 @@ const Contact = ({ data: { contact } }) => (
             __html: contact.pageTextNode.childMarkdownRemark.html,
           }}
         />
+        <div className="contact__form">
+          <Form
+            name="Contact form"
+            data-netlify="true"
+            data-netlify-recaptcha="true"
+          >
+            <input type="hidden" name="form-name" value="Contact form" />
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="name" />
+            </Form.Group>
+
+            <Form.Group controlId="formNumber">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="text" name="email" />
+            </Form.Group>
+
+            <Form.Group controlId="formDate">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows="3" name="message" />
+            </Form.Group>
+
+            <div data-netlify-recaptcha="true"></div>
+
+            <Button
+              variant="primary"
+              type="submit"
+              style={{
+                width: '8em',
+                backgroundColor: 'black',
+                marginTop: '40px',
+              }}
+            >
+              SEND
+            </Button>
+          </Form>
+        </div>
       </div>
     </article>
   </Layout>
