5787964ec3c7c5c3ded99db2985f3c4d5d40e37d
John Rickard <hello@johnrickard.me>
2020-03-03 03:30:12 +0100

    Fix oops

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 
 import '../styles/index.sass'
 import '../fonts/fontawesome/css/all.min.css'
